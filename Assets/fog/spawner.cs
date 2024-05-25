5f07c7c04a91368a566601386f413ba3f3fa8d84
https://gitlab.com/k.alaya9 <mayalove314@gmail.com>
2024-05-25 14:06:31 +0300

    adding wind power to project

--- Assets/fog/spawner.cs
+++ Assets/fog/spawner.cs
@@ -45,6 +45,8 @@ public class spawner : MonoBehaviour
 
     private Particle[] renderParticles;
 
+    public Vector3 windSpeed=new Vector3(0,0,0);
+
     void Awake()
     {
         InitParticles();
@@ -126,21 +128,27 @@ Vector3 spawnTopLeft = spawnBoxCenter - spawnBox / 2;
         kernelComputeForces = computeShader.FindKernel("ComputeForces");
         kernelIntegrate = computeShader.FindKernel("Integrate");
 
-        computeShader.SetFloat("gravity",gravity);
+        computeShader.SetInt("particleLength", numParticles);
         computeShader.SetFloat("particleMass", particleMass);
         computeShader.SetFloat("viscosity", viscosity);
         computeShader.SetFloat("gasConstant", gasConstant);
         computeShader.SetFloat("restDensity", restingDensity);
         computeShader.SetFloat("boundDamping", boundDamping);
+
         computeShader.SetFloat("radius", particleRadius);
         computeShader.SetFloat("radius2", particleRadius * particleRadius);
         computeShader.SetFloat("radius3", particleRadius * particleRadius * particleRadius);
         computeShader.SetFloat("radius4", particleRadius * particleRadius * particleRadius * particleRadius);
         computeShader.SetFloat("radius5", particleRadius * particleRadius * particleRadius * particleRadius * particleRadius);
-        computeShader.SetFloat("pi", 3.1415926535897932384626433832795028841971f);
+
+        computeShader.SetFloat("pi", Mathf.PI);
+        computeShader.SetFloat("densityWeightConstant", 0.00497359197162172924277761760539f);
+        computeShader.SetFloat("spikyGradient", -0.09947183943243458485555235210782f);
+        computeShader.SetFloat("viscLaplacian", 0.39788735772973833942220940843129f);
 
         computeShader.SetFloat("timestep", deltaTime);
         computeShader.SetVector("boxSize", boxSize);
+        computeShader.SetVector("windSpeed",windSpeed);
 
         computeShader.SetBuffer(kernelComputeDensityPressure, "_particles", particleBuffer);
         computeShader.SetBuffer(kernelComputeForces, "_particles", particleBuffer);
@@ -175,3 +183,190 @@ Vector3 spawnTopLeft = spawnBoxCenter - spawnBox / 2;
         }
     }
 }
+
+
+// using System.Runtime.InteropServices;
+// using System.Collections.Generic;
+// using UnityEngine;
+// [System.Serializable]
+// [StructLayout(LayoutKind.Sequential, Size = 44)]
+// public struct Particle
+// {
+//     public float pressure; // 4
+//     public float density; // 8
+//     public Vector3 currentForce; // 20
+//     public Vector3 velocity; // 32
+//     public Vector3 position; // 44
+// }
+// public class spawner : MonoBehaviour
+// {
+//     public ComputeShader computeShader;
+//     public Mesh particleMesh;
+//     public Material material;
+//    public Vector3Int numToSpawn = new Vector3Int(10,10,10);
+//    private int numParticles;
+//     public Vector3 spawnBoxCenter = new Vector3(0,3,0);
+//     public Vector3 spawnBox = new Vector3(4,2,1.5f);
+//     // public Transform spawnArea;
+//     public Vector3 boxSize;
+//     public float particleRadius = 0.1f;
+//     public float boundDamping = -0.3f;
+//     public float viscosity = -0.003f;
+//     public float particleMass = 1f;
+//     public float gasConstant = 2f; 
+//     public float restingDensity = 1f; 
+//     public float deltaTime = 0.0001f;
+//     public float gravity=-9.8f;
+//     private Particle[] particles;
+//     private ComputeBuffer particleBuffer;
+//     private ComputeBuffer argsBuffer;
+//     private Bounds bounds;
+//     private static readonly int SizeProperty = Shader.PropertyToID("_size");
+//     private static readonly int ParticlesBufferProperty = Shader.PropertyToID("_particles");
+
+//     private int kernelComputeDensityPressure;
+//     private int kernelComputeForces;
+//     private int kernelIntegrate;
+
+//     void Awake()
+//     {
+
+
+//         InitParticles();
+//         InitBuffers();
+//         bounds = new Bounds(Vector3.zero, boxSize);
+//     }
+
+//     void InitParticles()
+//     {
+// Vector3 spawnTopLeft = spawnBoxCenter - spawnBox / 2;
+//         List<Particle> _particles = new List<Particle>();
+
+//         for (int x = 0; x < numToSpawn.x; x++)
+//         {
+//             for (int y = 0; y < numToSpawn.y; y++)
+//             {
+//                 for (int z = 0; z < numToSpawn.z; z++)
+//                 {
+//                     Vector3 spawnPosition = spawnTopLeft + new Vector3(x * particleRadius * 2, y * particleRadius * 2, z * particleRadius * 2) + Random.onUnitSphere * particleRadius * 0.1f;
+//                     Particle p = new Particle
+//                     {
+//                         position = spawnPosition
+//                     };
+
+//                     _particles.Add(p);
+//                 }
+//             }
+//         }
+//         numParticles=_particles.Count;
+//         // Debug.Log(numParticles);
+//         particleBuffer = new ComputeBuffer(numParticles, 44);
+//         particleBuffer.SetData(_particles.ToArray());
+//         particles = _particles.ToArray();
+//     }
+
+//     void InitBuffers()
+//     {
+//         uint[] args = { particleMesh.GetIndexCount(0), (uint)numParticles, particleMesh.GetIndexStart(0), particleMesh.GetBaseVertex(0), 0 };
+//         argsBuffer = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
+//         argsBuffer.SetData(args);
+
+//         RunComputeShader();
+//     }
+
+//     private void FixedUpdate()
+//     {
+//         // RunComputeShader();
+//         // computeShader.SetFloat("timestep", deltaTime);
+//         // computeShader.SetVector("boxSize", boxSize);
+
+
+//         // computeShader.Dispatch(kernelComputeDensityPressure, numParticles / 100, 1, 1);
+//         // computeShader.Dispatch(kernelComputeForces, numParticles / 100, 1, 1);
+//         // computeShader.Dispatch(kernelIntegrate, numParticles / 100, 1, 1);
+
+//         // material.SetFloat(SizeProperty, 16.0f);
+//         // material.SetBuffer(ParticlesBufferProperty, particleBuffer);
+
+//         // Graphics.DrawMeshInstancedIndirect(particleMesh, 0, material, bounds, argsBuffer, castShadows: UnityEngine.Rendering.ShadowCastingMode.Off);
+//     }
+
+//     private void Update()
+//     {
+//         RunComputeShader();
+//         // particleBuffer.GetData(particles);
+//         // for (int i = 0; i < particles.Length; i++)
+//         // {
+//         //  Debug.Log("density "+i+" is :"+particles[i].density);   
+//         // }
+        
+//         Graphics.DrawMeshInstancedIndirect(particleMesh, 0, material, bounds, argsBuffer, castShadows: UnityEngine.Rendering.ShadowCastingMode.Off);
+//     }
+
+//     void RunComputeShader()
+//     {   
+//         kernelComputeDensityPressure = computeShader.FindKernel("ComputeDensityPressure");
+//         kernelComputeForces = computeShader.FindKernel("ComputeForces");
+//         kernelIntegrate = computeShader.FindKernel("Integrate");
+//         computeShader.SetInt("particleLength", numParticles);
+//         computeShader.SetFloat("particleMass", particleMass);
+//         computeShader.SetFloat("viscosity", viscosity);
+//         computeShader.SetFloat("gasConstant", gasConstant);
+//         computeShader.SetFloat("restDensity", restingDensity);
+//         computeShader.SetFloat("boundDamping", boundDamping);
+
+//         computeShader.SetFloat("radius", particleRadius);
+//         computeShader.SetFloat("radius2", particleRadius * particleRadius);
+//         computeShader.SetFloat("radius3", particleRadius * particleRadius * particleRadius);
+//         computeShader.SetFloat("radius4", particleRadius * particleRadius * particleRadius * particleRadius);
+//         computeShader.SetFloat("radius5", particleRadius * particleRadius * particleRadius * particleRadius * particleRadius);
+
+//         computeShader.SetFloat("pi", Mathf.PI);
+//         computeShader.SetFloat("densityWeightConstant", 0.00497359197162172924277761760539f);
+//         computeShader.SetFloat("spikyGradient", -0.09947183943243458485555235210782f);
+//         computeShader.SetFloat("viscLaplacian", 0.39788735772973833942220940843129f);
+
+
+//         computeShader.SetVector("boxSize", boxSize);
+
+//         computeShader.SetFloat("timestep", deltaTime);
+
+//         computeShader.SetBuffer(kernelComputeDensityPressure, "_particles", particleBuffer);
+//         computeShader.SetBuffer(kernelComputeForces, "_particles", particleBuffer);
+//         computeShader.SetBuffer(kernelIntegrate, "_particles", particleBuffer);
+
+
+//         int threadGroups = Mathf.CeilToInt(numParticles / 100.0f);
+
+//         computeShader.Dispatch(kernelComputeDensityPressure, numParticles, 1, 1);
+//         computeShader.Dispatch(kernelComputeForces, numParticles, 1, 1);
+//         computeShader.Dispatch(kernelIntegrate, numParticles, 1, 1);
+
+//         material.SetFloat(SizeProperty, 16.0f);
+//         material.SetBuffer(ParticlesBufferProperty, particleBuffer);
+//     }
+
+//     // void OnDestroy()
+//     // {
+//     //     if (particleBuffer != null)
+//     //     {
+//     //         particleBuffer.Release();
+//     //     }
+
+//     //     if (argsBuffer != null)
+//     //     {
+//     //         argsBuffer.Release();
+//     //     }
+//     // }
+//         private void OnDrawGizmos()
+//     {
+//         Gizmos.color = Color.blue;
+//         Gizmos.DrawWireCube(Vector3.zero, boxSize);
+
+//         if (!Application.isPlaying)
+//         {
+//             Gizmos.color = Color.cyan;
+//             Gizmos.DrawWireCube(spawnBoxCenter, spawnBox);
+//         }
+//     }
+// }
